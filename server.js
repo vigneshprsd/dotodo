ccced9e056292cf5009e1ee9c92ea852d4680a17
vigneshprsd <vigneshprsd@gmail.com>
2020-10-03 16:22:20 +0530

    first commit3

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ app.use('/api/todo',require('./routes/api/todo'));
 //serve static assets in production
 if(process.env.NODE_ENV==='production'){
     //set static folder
-    app.use(express.static('client/build'));
+    app.use(express.static('todo/build'));
     app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(_dirname,'client','build','index.html'));
+        res.sendFile(path.resolve(__dirname,'todo','build','index.html'));
     });
 
 }
